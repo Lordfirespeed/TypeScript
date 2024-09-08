77d6f4b0762f1ec681be254486f3190b5d616e18
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2024-09-08 05:51:16 +0100

    feat: add `Object.keys` overload which retains type information relating to keys

--- src/lib/es5.d.ts
+++ src/lib/es5.d.ts
@@ -243,6 +243,12 @@ interface ObjectConstructor {
      * Returns the names of the enumerable string properties and methods of an object.
      * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
      */
+    keys<K extends string | number | symbol>(o: Record<K, unknown>): K[]
+
+    /**
+     * Returns the names of the enumerable string properties and methods of an object.
+     * @param o Object that contains the properties and methods. This can be an object that you created or an existing Document Object Model (DOM) object.
+     */
     keys(o: object): string[];
 }
 
